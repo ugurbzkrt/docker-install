e86fe36fb197f40610f9769e7b93ed89e2c82117
Ugur Bozkurt <94750332+ugurbzkrt@users.noreply.github.com>
2023-07-17 14:23:37 +0300

    Update ubunu22.04.sh

--- ubunu22.04.sh
+++ ubunu22.04.sh
@@ -18,7 +18,6 @@ sudo systemctl enable docker && sudo systemctl start docker
 
 sudo usermod -aG docker ${USER}
 
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-chmod +x ~/.docker/cli-plugins/docker-compose
