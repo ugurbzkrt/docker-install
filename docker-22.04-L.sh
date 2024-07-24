8b710514bb8011b2d1112f1870e2e501986f3bf9
Ugur Bozkurt <94750332+ugurbzkrt@users.noreply.github.com>
2024-07-24 15:12:25 +0300

    Update docker-22.04-L.sh

--- docker-22.04-L.sh
+++ docker-22.04-L.sh
@@ -18,7 +18,7 @@ sudo systemctl enable docker
 
 sudo usermod -aG docker ${USER}
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
