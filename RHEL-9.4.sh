abbad19a51007f4987f7a8ceec0717ea37f5ec23
Ugur Bozkurt <94750332+ugurbzkrt@users.noreply.github.com>
2024-08-12 11:43:13 +0300

    Create RHEL-9.4.sh

---
+++ RHEL-9.4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo yum update -y
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine \
+                  podman \
+                  runc
+
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+systemctl start docker
+
+sudo systemctl enable docker
+
+sudo usermod -aG docker ${USER}
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
