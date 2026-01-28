de8b7ff217fcb32c5c62a306030649a1a8a7234c
Ugur Bozkurt <94750332+ugurbzkrt@users.noreply.github.com>
2026-01-28 14:54:24 +0300

    Update 24.04-LTS-docker.sh

--- 24.04-LTS-docker.sh
+++ 24.04-LTS-docker.sh
@@ -15,5 +15,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo systemctl enable docker
 sudo usermod -aG docker ${USER}
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+LATEST_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+sudo curl -L "https://github.com/docker/compose/releases/download/${LATEST_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
